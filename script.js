d099e41d4f3dcb817782f041ca1fdd03cc8ff7cc
Im-Dilasna-real <112766883+Im-Dilasna-real@users.noreply.github.com>
2025-01-14 19:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Student"],
-        typeSpeed: 100,
+        typeSpeed: 60,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
         strings: ["Student"],
-        typeSpeed: 100,
+        typeSpeed: 60,
         backSpeed: 60,
         loop: true
     });
